4559d4188ab0b193f188ca38f94acb1a66e8b99b
Slava Babin <babinslava@gmail.com>
2017-01-28 23:26:37 +0200

    HelloWorldMongoSparkFreemarker.java is completed.

--- src/main/java/biz/bsoft/HelloWorldMongoSparkFreemarker.java
+++ src/main/java/biz/bsoft/HelloWorldMongoSparkFreemarker.java
@@ -1,21 +1,26 @@
 package biz.bsoft;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.bson.Document;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
 
 import java.io.File;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * Created by vbabin on 1/28/2017.
- */
 public class HelloWorldMongoSparkFreemarker {
     public static void main(String[] args) {
+        MongoClient  client=new MongoClient();
+        MongoDatabase database= client.getDatabase("test");
+        MongoCollection<Document> collection=database.getCollection("names");
+        collection.drop();
+        collection.insertOne(new Document("name","bsoft.biz"));
+
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
 
         Spark.get("/", (Request req, Response res) ->{
@@ -23,10 +28,8 @@ public class HelloWorldMongoSparkFreemarker {
             try {
                 cfg.setDirectoryForTemplateLoading(new File("/projects/mongoJ/src/main/resources/"));
                 Template template= cfg.getTemplate("index.ftl");
-                Map<String, Object> helloMap = new HashMap<>();
-                helloMap.put("name","bsoft.biz");
-                template.process(helloMap,writer);
-                //System.out.println(writer);
+                Document document = collection.find().first();
+                template.process(document,writer);
             }
             catch (Exception e)
             {
